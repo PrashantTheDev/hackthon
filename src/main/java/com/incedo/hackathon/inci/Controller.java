871543827620488f480a231c30b5bcd62558dd41
Prashant Prakash Nandan <TSS\pnandan@a-2vcfhiiwy4gbs.tripwirevdi.com>
2021-11-26 12:10:39 +0530

    First Commit

---
+++ src/main/java/com/incedo/hackathon/inci/Controller.java
@@ -0,0 +1,10 @@
+package com.incedo.hackathon.inci;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+
+}
---
+++ src/test/java/com/incedo/hackathon/inci/InciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incedo.hackathon.inci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
